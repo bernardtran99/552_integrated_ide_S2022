23b4bfd678c49d9187c9e12be8af8b66283b36e4
bernardtran99 <bernardtran99@gmail.com>
2022-04-11 17:19:53 -0400

    update all

--- GUI.java
+++ GUI.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 public class GUI {
     public static void main(String[] args) {
         System.out.println("Starting GUI...");
+        JFrame frame = new JFrame("Java IDE");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        JLabel label = new JLabel("This is a Java IDE");
+        frame.getContentPane().add(label);
+
+        frame.setVisible(true);
     }
 }
