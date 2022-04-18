ec1ebfe3dd5bb8b934f5f21d9216793d828da5d9
bernardtran99 <bernardtran99@gmail.com>
2022-04-18 13:42:42 -0400

    update all

--- sample/Text.java
+++ sample/Text.java
@@ -12,21 +12,19 @@ public class Text extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample1.fxml"));
-        TextField tf1=new TextField();
+        TextField tf1 = new TextField();
         Button b = new Button("Run");
-//        GridPane root = new GridPane();
-//        root.addRow(0,tf1);
-//        root.addRow(1,b);
-        Scene scene=new Scene(root,800,200);
+        // GridPane root = new GridPane();
+        // root.addRow(0,tf1);
+        // root.addRow(1,b);
+        Scene scene = new Scene(root, 800, 200);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Text Field Example");
         primaryStage.show();
 
-
-
-
-}
+    }
 }
